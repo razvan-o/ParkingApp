746091533a17ccc92332aca524b8a0cfb36d43c1
Razvan Olariu <razvan@filed.com>
2021-03-03 09:35:32 +0200

    improved indentation of errora messages

--- PayParkingLibrary/Validators/ErrorMessages.cs
+++ PayParkingLibrary/Validators/ErrorMessages.cs
@@ -5,13 +5,13 @@
 		private static readonly string AdminContact = "Please seek parking adinistrator for assistance. Mobile: 0711 222 333";
 
 		// Possible issue with camera reader; can implement sending notification to parking administrator
-		public static readonly string DuplicateRegistrationNumber = $"Car with same registration number already in parking lot. {AdminContact}";
+		public static readonly string DuplicateRegistrationNumber = $"Car with same registration number already in parking lot.\n\t{AdminContact}";
 
 		// Possible issue with camera reader; can implement sending notification to parking administrator
-		public static readonly string RegistrationNumberNotFound = $"Car registration number is not recognized. {AdminContact}";
+		public static readonly string RegistrationNumberNotFound = $"Car registration number is not recognized.\n\t{AdminContact}";
 
 		// Software issue; can implement sending notification to parking administrator
-		public static readonly string InvalidTimestamps = $"We are facing some internal issues. {AdminContact}";
+		public static readonly string InvalidTimestamps = $"We are facing some internal issues.\n\t{AdminContact}";
 
 		// Can implement flow to collect client data and notify him when a parking spot becomes available
 		public static readonly string NoAvailableSpots = "There are no spots available";
